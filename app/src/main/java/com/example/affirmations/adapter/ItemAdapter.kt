e94b18cf1723a99136a479560e1a97430df12e88
Sourabh kumar <20010001030@gateway.edu.in>
2022-03-18 13:47:24 +0530

    added Images

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -21,6 +22,7 @@ class ItemAdapter(private val context: Context,
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     //Create new views (invoked by the layout manager)
@@ -37,6 +39,7 @@ class ItemAdapter(private val context: Context,
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     //Return the size of your dataset (invoked by the layout manager)
